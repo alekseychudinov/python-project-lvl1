4519c1c86d75dbed11fc370fea128f5c6f170f4d
AlekseyChudinov <alekseychudinov@gmail.com>
2022-01-26 21:19:42 +0500

    new version v.4

--- brain_games/greetings.py
+++ brain_games/greetings.py
@@ -1,18 +1,9 @@
 import prompt
 
 
-def greetings(game):
+def greetings(greeting):
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print("Hello, {0}!".format(name))
-    if game == "calc":
-        print("What is the result of the expression?")
-    elif game == "even":
-        print('Answer "yes" if the number is even, otherwise answer "no".')
-    elif game == "gcd":
-        print("Find the greatest common divisor of given numbers.")
-    elif game == "prime":
-        print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    elif game == "progression":
-        print("What number is missing in the progression?")
+    print(greeting)
     return name
