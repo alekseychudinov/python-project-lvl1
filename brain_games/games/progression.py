169237a217c8cd5e1a50d12441e08ffcdf85bc02
AlekseyChudinov <alekseychudinov@gmail.com>
2022-02-01 20:16:07 +0500

    new version v.8

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,7 +12,24 @@ INITIAL_TERM_MIN = 1
 INITIAL_TERM_MAX = 100
 
 
-def progression_parameters():
+def calculate_progression_terms(
+    initial_term, stop_progression, common_difference
+):
+    progression = []
+    for element in range(initial_term, stop_progression, common_difference):
+        progression.append(element)
+    return progression
+
+
+def stringify_progression(progression, hidden_term_index):
+    for index, value in enumerate(progression):
+        progression[index] = str(value)
+    progression[hidden_term_index] = ".."
+    str_progression = " ".join(progression)
+    return str_progression
+
+
+def get_correct_answer_and_question():
     length_progression = random.randint(LENGTH_MIN, LENGTH_MAX)
     common_difference = random.randint(
         COMMON_DIFFERENCE_MIN, COMMON_DIFFERENCE_MAX
@@ -20,30 +37,10 @@ def progression_parameters():
     initial_term = random.randint(INITIAL_TERM_MIN, INITIAL_TERM_MAX)
     stop_progression = initial_term + common_difference * length_progression
     hidden_term_index = random.randint(0, length_progression - 1)
-    return initial_term, stop_progression, common_difference, hidden_term_index
 
-
-def calculate_progression_terms():
-    (
-        initial_term,
-        stop_progression,
-        common_difference,
-        hidden_term_index,
-    ) = progression_parameters()
-    progression = []
-    for element in range(initial_term, stop_progression, common_difference):
-        progression.append(str(element))
-    return progression, hidden_term_index
-
-
-def stringify_progression():
-    progression, hidden_term_index = calculate_progression_terms()
-    hidden_term = progression[hidden_term_index]
-    progression[hidden_term_index] = ".."
-    str_progression = " ".join(progression)
-    return str(hidden_term), str_progression
-
-
-def get_correct_answer_and_question():
-    correct_answer, question = stringify_progression()
+    progression = calculate_progression_terms(
+        initial_term, stop_progression, common_difference
+    )
+    correct_answer = str(progression[hidden_term_index])
+    question = stringify_progression(progression, hidden_term_index)
     return correct_answer, question
